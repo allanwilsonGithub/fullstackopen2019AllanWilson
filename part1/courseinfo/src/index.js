0ed0cee6efa1f51a775fdab686ae15babe23cf3b
Allan Wilson <allan@allanwilson.net>
2019-09-23 22:55:58 +0300

    Part 1.2 Course information Step2 DONE

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -14,16 +14,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+      <p>
+        {props.part} {props.exercises}
+      </p>
   )
 }
 
@@ -36,6 +38,7 @@ const Total = (props) => {
     </div>
   )
 }
+
 
 const App = () => {
   // const-definitions
