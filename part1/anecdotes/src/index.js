9806cfb34bcd830f79776d22bf01434b781a0f36
Allan Wilson <allan@allanwilson.net>
2019-10-08 03:42:28 +0300

    Nearly done. Logic not quite right

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -6,24 +6,31 @@ import ReactDOM from 'react-dom'
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [mostVotes, setMostVotes] = useState(0)
 
   const changeAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
   const addVoteForAnecdote = () => {
+    console.log(mostVotes, votes)
     const copy = [...votes]
-    copy[selected]++
+    copy[selected] += 1
     setVotes(copy)
+    setMostVotes(votes.indexOf(Math.max(...votes)))
+    console.log(mostVotes, votes)
   }
 
   return (
     <div>
+      <h1>Anecdote of the day...</h1>
       {props.anecdotes[selected]}
       <p>.. has {votes[selected]} votes.</p>
       <button onClick={addVoteForAnecdote}>Vote for this anecdote ...</button>
       <br></br>
       <button onClick={changeAnecdote}>Next anecdote ...</button>
+      <h1>Anecdote with most votes...{Math.max(...votes)}</h1>
+      {props.anecdotes[mostVotes]}
     </div>
   )
 }
