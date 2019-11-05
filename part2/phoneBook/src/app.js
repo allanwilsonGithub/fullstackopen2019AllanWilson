8c091c246ecae3582987318f9f7f598861d63d5b
Allan Wilson <allan@allanwilson.net>
2019-11-06 00:36:23 +0200

    Next check for dups

--- part2/phoneBook/src/app.js
+++ part2/phoneBook/src/app.js
@@ -2,36 +2,23 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    'Arto Hellas'
-  ])
-
-  const [ numbers, setNumbers] = useState([
-    '01382 730 426'
+    { name: 'Arto Hellas', number: '040-123456' },
   ])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
 
-  const DisplayNames = ({persons}) => persons.map((person, i) =>
-      <p key={person}>{person} {numbers[i]}</p>
+  const DisplayNames = ({persons}) => persons.map((entry, i) =>
+      <p key={i}>{entry.name} {entry.number}</p>
   )
 
   const addPerson = (event) => {
       event.preventDefault()
-      if (persons.indexOf(newName) > 0) {
+      if (persons.indexOf('name' === newName) > 0) {
           window.alert(`${newName} already exists in the phonebook`)
       } else {
-          setPersons(persons.concat(newName))
-          setNumbers(numbers.concat(newNumber))
-          setNewName('')
-          setNewNumber('')
+          setPersons([...persons, { name: newName, number: newNumber }])
       }
-  }
-
-  const addNumber = (event) => {
-      event.preventDefault()
-      setNumbers(numbers.concat(newNumber))
-      setNewNumber('')
   }
 
   const handleNameChange = (event) => {
