181ac7edd9aad52d0f372875174cf6f64bae9a4a
Allan Wilson <allan@allanwilson.net>
2019-11-06 23:46:42 +0200

    Part 2.10 ongoing

---
+++ part2/phoneBook/src/components/displayNames.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+  const DisplayNames = ({ persons, newFilterString }) => persons.map((entry, i) => {
+      if (entry.name.toLowerCase().includes(newFilterString.toLowerCase()))
+          return (
+          <p key={i}>{entry.name} {entry.number}</p>
+          )
+        }
+   )
+
+export default DisplayNames
