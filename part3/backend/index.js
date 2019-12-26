d37dcf87dfb649e2ef1d561d0e27cd56031cbf8c
Allan Wilson <allan@allanwilson.net>
2019-12-26 23:36:27 +0200

    Part3 3.22: Lint configuration DONE

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
 
 let notes = [
   {
@@ -29,6 +34,55 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.json(notes)
 })
+
+app.get('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
